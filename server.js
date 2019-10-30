ff1b83e7030c519f3463243edcf16805d2fad9cf
kang.chen <kang.chen@coupang.com>
2019-10-30 13:16:00 +0800

    server file

--- server.js
+++ server.js
@@ -1,87 +1,87 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 let port = process.argv[2] || 3000;
 const httpServer = http.createServer(requestHandler);
-httpServer.listen(port, () => {console.log('server is listening on port '+ port)});
+httpServer.listen(port, () => {
+  console.log("server is listening on port " + port);
+});
 
-function requestHandler(req, res){
-  if(req.url === '/'){
+function requestHandler(req, res) {
+  if (req.url === "/") {
     sendIndexHtml(res);
-  }else if( req.url === '/list'){
+  } else if (req.url === "/list") {
     sendListOfUploadedFiles(res);
-  }else if( /\/download\/[^\/]+$/.test(req.url)){
+  } else if (/\/download\/[^\/]+$/.test(req.url)) {
     sendUploadedFile(req.url, res);
-  }else if( /\/upload\/[^\/]+$/.test(req.url) ){
-    saveUploadedFile(req, res)
-  }else{
+  } else if (/\/upload\/[^\/]+$/.test(req.url)) {
+    saveUploadedFile(req, res);
+  } else {
     sendInvalidRequest(res);
   }
 }
 
-function sendIndexHtml(res){
-  let indexFile = path.join(__dirname, 'index.html');
+function sendIndexHtml(res) {
+  let indexFile = path.join(__dirname, "index.html");
   fs.readFile(indexFile, (err, content) => {
-    if(err){
-      res.writeHead(404, {'Content-Type': 'text'});
-      res.write('File Not Found!');
+    if (err) {
+      res.writeHead(404, { "Content-Type": "text" });
+      res.write("File Not Found!");
       res.end();
-    }else{
-      res.writeHead(200, {'Content-Type': 'text/html'});
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
       res.write(content);
       res.end();
     }
-  })
+  });
 }
 
-function sendListOfUploadedFiles(res){
-  let uploadDir = path.join(__dirname, 'download');
+function sendListOfUploadedFiles(res) {
+  let uploadDir = path.join(__dirname, "download");
   fs.readdir(uploadDir, (err, files) => {
-    if(err){
+    if (err) {
       console.log(err);
-      res.writeHead(400, {'Content-Type': 'application/json'});
+      res.writeHead(400, { "Content-Type": "application/json" });
       res.write(JSON.stringify(err.message));
       res.end();
-    }else{
-      res.writeHead(200, {'Content-Type': 'application/json'});
+    } else {
+      res.writeHead(200, { "Content-Type": "application/json" });
       res.write(JSON.stringify(files));
       res.end();
     }
-  })
+  });
 }
 
-
-function sendUploadedFile(url, res){
+function sendUploadedFile(url, res) {
   let file = path.join(__dirname, url);
   fs.readFile(file, (err, content) => {
-    if(err){
-      res.writeHead(404, {'Content-Type': 'text'});
-      res.write('File Not Found!');
+    if (err) {
+      res.writeHead(404, { "Content-Type": "text" });
+      res.write("File Not Found!");
       res.end();
-    }else{
-      res.writeHead(200, {'Content-Type': 'application/octet-stream'});
+    } else {
+      // res.writeHead(200, {'Content-Type': 'application/octet-stream'});
       res.write(content);
       res.end();
     }
-  })
+  });
 }
 
-
-function saveUploadedFile(req, res){
-  console.log('saving uploaded file');
+function saveUploadedFile(req, res) {
+  console.log("saving uploaded file");
   let fileName = path.basename(req.url);
-  let file = path.join(__dirname, 'download', fileName)
+  let file = path.join(__dirname, "download", fileName);
   req.pipe(fs.createWriteStream(file));
-  req.on('end', () => {
-    res.writeHead(200, {'Content-Type': 'text'});
-    res.write('uploaded succesfully');
+  req.on("end", () => {
+    res.writeHead(200, { "Content-Type": "text" });
+    res.write("uploaded succesfully");
     res.end();
-  })
+  });
 }
 
-function sendInvalidRequest(res){
-  res.writeHead(400, {'Content-Type': 'application/json'});
-  res.write('Invalid Request');
-  res.end(); 
+function sendInvalidRequest(res) {
+  res.writeHead(400, { "Content-Type": "application/json" });
+  res.write("Invalid Request");
+  res.end();
 }
